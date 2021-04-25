eac30af1b2cd19814381b33290dbb71a9b310283
robmaz22 <czowieg@gmail.com>
2021-04-25 18:28:41 +0200

    dodanie przekazywania argumentu ze ścieżką do katalogu

--- preprocesing.py
+++ preprocesing.py
@@ -1,28 +1,32 @@
 import shutil
 import os
 from random import shuffle
+import argparse
 
+parser = argparse.ArgumentParser(description='Preparing data for neural network')
+parser.add_argument('-p', "--path", help="Path to images directory")
+args = parser.parse_args()
 
-filename = 'images.zip'
-shutil.unpack_archive(filename)
+zip_path = args.path
+shutil.unpack_archive(zip_path, '/'.join(zip_path.split('/')[:-1]))
 
+file_path = zip_path.split('.')[0]
 classes = ['frog', 'cow']
 
 for class_ in classes:
     i = 1
-    for name in os.listdir(f'images/{class_}'):
-        print(name)
-        os.rename(f'images/{class_}/{name}', f'images/{class_}/{i:04d}.jpg')
+    for name in os.listdir(f'{file_path}/{class_}'):
+        os.rename(f'{file_path}/{class_}/{name}', f'{file_path}/{class_}/{i:04d}.jpg')
         i += 1
 
 dirs = ['train', 'test', 'valid']
-base_dir = 'data'
+base_dir = f'{file_path}/data'
 
 for dir in dirs:
     for class_ in classes:
         os.makedirs(os.path.join(base_dir, dir, class_))
 
-number_of_pictures = len(os.listdir('images/cow'))
+number_of_pictures = len(os.listdir(f'{file_path}/cow'))
 
 train_size = int(number_of_pictures * 0.7)
 test_size = int(number_of_pictures * 0.2)
@@ -32,25 +36,25 @@ print(f'Liczba danych do trenowania: {train_size}')
 print(f'Liczba danych do testu: {test_size}')
 print(f'Liczba danych do walidacji: {valid_size}')
 
-frog_fnames = [fname for fname in os.listdir('images/frog')]
-cow_fnames = [fname for fname in os.listdir('images/cow')]
+frog_fnames = [fname for fname in os.listdir(f'{file_path}/frog')]
+cow_fnames = [fname for fname in os.listdir(f'{file_path}/cow')]
 
 shuffle(frog_fnames)
 shuffle(cow_fnames)
 
-lists = {'frog':frog_fnames,
-         'cow':cow_fnames}
+lists = {'frog': frog_fnames,
+         'cow': cow_fnames}
 
 for class_ in classes:
     idx = 0
     while idx < 400:
 
-      scr = os.path.join('images', class_, lists[class_][idx])
-      if idx < train_size:
-        dst = os.path.join('data', 'train', class_, lists[class_][idx])
-      elif idx > train_size and idx =< (train_size + test_size):
-        dst = os.path.join('data', 'test', class_, lists[class_][idx])
-      else:
-        dst = os.path.join('data', 'valid', class_, lists[class_][idx])
-      shutil.copy(scr, dst)
-      idx += 1
+        scr = os.path.join(file_path, class_, lists[class_][idx])
+        if idx < train_size:
+            dst = os.path.join(file_path, 'data', 'train', class_, lists[class_][idx])
+        elif idx > train_size and idx <= (train_size + test_size):
+            dst = os.path.join(file_path, 'data', 'test', class_, lists[class_][idx])
+        else:
+            dst = os.path.join(file_path, 'data', 'valid', class_, lists[class_][idx])
+        shutil.copy(scr, dst)
+        idx += 1
