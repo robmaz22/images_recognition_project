0b80dd6227526b186b57f2a9cde211a8848d93cb
robmaz22 <czowieg@gmail.com>
2021-04-25 18:05:19 +0200

    podział zdjęć na odpowiednie foldery

--- preprocesing.py
+++ preprocesing.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+from random import shuffle
 
 
 filename = 'images.zip'
@@ -21,6 +22,35 @@ for dir in dirs:
     for class_ in classes:
         os.makedirs(os.path.join(base_dir, dir, class_))
 
+number_of_pictures = len(os.listdir('images/cow'))
 
+train_size = int(number_of_pictures * 0.7)
+test_size = int(number_of_pictures * 0.2)
+valid_size = int(number_of_pictures * 0.1)
 
+print(f'Liczba danych do trenowania: {train_size}')
+print(f'Liczba danych do testu: {test_size}')
+print(f'Liczba danych do walidacji: {valid_size}')
 
+frog_fnames = [fname for fname in os.listdir('images/frog')]
+cow_fnames = [fname for fname in os.listdir('images/cow')]
+
+shuffle(frog_fnames)
+shuffle(cow_fnames)
+
+lists = {'frog':frog_fnames,
+         'cow':cow_fnames}
+
+for class_ in classes:
+    idx = 0
+    while idx < 400:
+
+      scr = os.path.join('images', class_, lists[class_][idx])
+      if idx < train_size:
+        dst = os.path.join('data', 'train', class_, lists[class_][idx])
+      elif idx > train_size and idx =< (train_size + test_size):
+        dst = os.path.join('data', 'test', class_, lists[class_][idx])
+      else:
+        dst = os.path.join('data', 'valid', class_, lists[class_][idx])
+      shutil.copy(scr, dst)
+      idx += 1
